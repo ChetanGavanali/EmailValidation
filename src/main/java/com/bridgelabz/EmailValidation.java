3272fa7ac4e5f48d5103bd218c61d7fb81ebae67
ChetanGavanali <chetangavanali@gmail.com>
2022-07-07 23:09:37 +0530

    Added UC1

--- src/main/java/com/bridgelabz/EmailValidation.java
+++ src/main/java/com/bridgelabz/EmailValidation.java
@@ -1,7 +1,34 @@
 package com.bridgelabz;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EmailValidation {
+    public static void checkValidEmail(String email){
+
+        boolean isEmail;
+        String emailRegex = "^[a-zA-Z][.@a-zA-Z]*$";
+        Pattern patternObject = Pattern.compile(emailRegex);
+        if (email == null) {
+            isEmail = false;
+        }
+        Matcher matcherObject = patternObject.matcher(email);
+        isEmail =  matcherObject.matches();
+
+        if(isEmail)
+            System.out.println(email+" is a Valid Email address");
+        else
+            System.out.println(email+" is Invalid Email address");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome To Email Validation Program");
+
+        System.out.println(" Welcome To Email Validation Program");
+
+        String email = "abc.xyz@bridgelabz.co.in";
+        checkValidEmail(email);
+
+        email = ".abc@abc.com";
+        checkValidEmail(email);
     }
 }
